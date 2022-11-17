f1748898c342d25920d3907c8898c708d15e0e20
ibsen4 <115497034+ibsen4@users.noreply.github.com>
2022-11-17 22:30:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-
 using namespace std;
+
 int main() {
     float a=0, b=0;
     int opzione=0;
@@ -9,16 +9,14 @@ int main() {
         case 0 :
         cout<< (a*b)/2;
         break;
-
-        case 1 :
+    case 1 :
         cout<< a*a;
         break;
-
-        case 2 :
+    case 2 :
         cout<< a*b;
         break;
-        default:
-            cout<<"opzione non valida";
+    default:
+        cout<<"opzione non valida";
     }
     return 0;
 }
